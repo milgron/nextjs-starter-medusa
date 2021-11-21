4f5a2c2f14c3384974d50269c58227d7e66e4eaa
Tomás Milgron <milgron@gmail.com>
2021-11-21 18:01:11 -0300

    Replace traditional img tag with nextjs Image

--- components/storefront/grid-single-product.jsx
+++ components/storefront/grid-single-product.jsx
@@ -1,23 +1,39 @@
 import styles from "../../styles/grid-single-product.module.css";
+import Link from "next/link";
+import Image from 'next/image'
 
 export const GridSingleProduct = ({data}) => {
     return(
-        <>
-            <div className={styles.product_card}>
-                <div className={styles.product_image_wrapper}>
-                    <img className={styles.product_image} src={data.thumbnail} />
-                </div>
-                <div className={styles.product_data_wrapper}>
-                    <div className={styles.product_title_wrapper}>
-                        <p className={styles.product_title}>{data.title}</p>
+        <Link
+            href={{ pathname: `/product/${data.id}`}}
+            passHref
+        >
+            <a>
+                <div className={styles.product_card}>
+                    <div className={styles.product_image_wrapper}>
+                        <Image
+                            objectFit="cover"
+                            height="100%"
+                            width="100%"
+                            priority={true}
+                            loading="eager"
+                            src={product.thumbnail}
+                            className={styles.product_image}
+                            alt={`${product.title}`}
+                        />
                     </div>
-                    <div className={styles.product_price_wrapper}>
-                        <div className={styles.product_price}>
-                            <p><span className={styles.price_from}>from</span> {data.lower_price}</p>
+                    <div className={styles.product_data_wrapper}>
+                        <div className={styles.product_title_wrapper}>
+                            <p className={styles.product_title}>{data.title}</p>
+                        </div>
+                        <div className={styles.product_price_wrapper}>
+                            <div className={styles.product_price}>
+                                <p><span className={styles.price_from}>from</span> {data.lower_price}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
+            </a>
+        </Link>
     )
 }
