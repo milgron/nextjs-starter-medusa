b5d6c063e50efb884db122292ce4426b765431b1
Tomás Milgron <milgron@gmail.com>
2021-11-21 19:26:58 -0300

    Adjust navbar to the design

--- components/layout/nav-bar.jsx
+++ components/layout/nav-bar.jsx
@@ -4,7 +4,7 @@ import React, { useContext, useEffect, useState } from "react";
 import DisplayContext from "../../context/display-context";
 import StoreContext from "../../context/store-context";
 import { quantity, sum } from "../../utils/helper-functions";
-import { BiShoppingBag } from "react-icons/bi";
+import { BiMenu } from "react-icons/bi";
 import styles from "../../styles/nav-bar.module.css";
 import { useRouter } from "next/router";
 import MedusaLogo from "../../public/medusa-logo.svg"
@@ -26,22 +26,19 @@ export const NavBar = () => {
 
   return (
     <div className={styles.container}>
+      {!isCheckout ? (
+        <button className={styles.btn} onClick={() => updateCartViewDisplay()}>
+          <BiMenu size="20px"/>{" "}
+          {/* <span>
+            {cart.items.length > 0 ? cart.items.map(quantity).reduce(sum) : 0}
+          </span> */}
+        </button>
+      ) : null}
       <Link href="/">
-        <a style={{ width: "125px" }}>
+        <a style={{ width: "125px" }} className={styles.navbarLogo}>
         <Image src={MedusaLogo} height="40px" width="100%" alt="logo" />
         </a>
       </Link>
-      {!isCheckout ? (
-        <button className={styles.btn} onClick={() => updateCartViewDisplay()}>
-          <span>
-            Cart
-          </span>
-          <BiShoppingBag />{" "}
-          <span>
-            {cart.items.length > 0 ? cart.items.map(quantity).reduce(sum) : 0}
-          </span>
-        </button>
-      ) : null}
     </div>
   );
 };
